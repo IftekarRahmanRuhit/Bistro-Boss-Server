840bc60b219e24dde24fd9c6c6bf1fa2e315246b
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2025-01-09 20:30:07 +0600

    added functionality for verifyAdmin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middlewares
@@ -31,12 +32,110 @@ async function run() {
     const reviewCollection = db.collection("reviews");
     const cartCollection = db.collection("carts");
 
+
+        // verifyToken  
+        const verifyToken = (req, res, next) => {
+          console.log('inside verify token', req.headers.authorization);
+          if (!req.headers.authorization) {
+            return res.status(401).send({ message: 'unauthorized access' });
+          }
+          const token = req.headers.authorization.split(' ')[1];
+          jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+              return res.status(401).send({ message: 'unauthorized access' })
+            }
+            req.decoded = decoded;
+            next();
+          })
+        }
+
+      //  admin   
+      app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+        // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+
+
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+          console.log(req.headers)
+          const user = req.body;
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+          res.send({ token });
+        })
+
+
+
+
+
     // user related api (saving user information) 
-    app.post("/users", async (req, res) => {
+    app.post('/users', async (req, res) => {
       const user = req.body;
+      // insert email if user doesnt exists: 
+      // we can do this many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+        // getting all user data from db
+        app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+         
+            console.log(req.headers)
+          const result = await userCollection.find().toArray();
+          res.send(result);
+        });
+
+        // making a user admin
+        app.patch('/users/admin/:id',verifyToken,verifyAdmin, async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) };
+          const updatedDoc = {
+            $set: {
+              role: 'admin'
+            }
+          }
+          const result = await userCollection.updateOne(filter, updatedDoc);
+          res.send(result);
+        })
+
+        
+        // delete a user 
+        app.delete('/users/:id', verifyToken,verifyAdmin, async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await userCollection.deleteOne(query);
+          res.send(result);
+        })
 
 
     // getting all menu data from db
